4ce8c8a938e16e96d9ee60aebe75cf8c947c9595
Admin <qwerty901cx@gmail.com>
2024-07-24 03:46:28 +0300

    Пару методов из интернета и всё готово.

--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -5,5 +5,11 @@ public class Main {
     // Коды даны для примера и не соответствуют действительности. Учитывайте, что нужно распечатать коды букв в двух регистрах!
     public static void main(String[] args) {
         //todo код писать тут
+       String alphabet = "ёйцукенгшщзхъфывапролджэячсмитьбюЁЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ";
+        for (int i = 0; i < alphabet.length(); i++) {
+            alphabet.codePointBefore(1);
+            System.out.println(alphabet.charAt(i) + "-" +  alphabet.codePointBefore(i+1));
+        }
     }
+
 }
--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
+    public static final String CORRECT_CHARS = " -ЁЙЦУКЕНГШЩЗХЪЭЖДЛОРПАВЫФЯЧСМИТЬБЮёйцукенгшщзхъэждлорпавыфячсмитьбю";
     public static void main(String[] args) {
         //todo код писать тут. По желанию(рекомендуется) вынести код в методы.
+        String input = new Scanner(System.in).nextLine().trim();
+        boolean isValid = true;
+        int spaceCount = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            if(input.charAt(i)==' '){
+                spaceCount++;
+            }
+            if(!CORRECT_CHARS.contains(String.valueOf(input.charAt(i)))){
+                isValid = false;
+                break;
+            }
+        }
+        if (isValid && spaceCount == 2){
+            System.out.println("Фамилия: " + input.replaceAll("\\s.*", "") +
+                    "\nИмя: " + input.substring(input.indexOf(' '), input.lastIndexOf(' ')) +
+                    "\nОтчество: " + input.substring(input.lastIndexOf(' ')));
+        }else {
+            System.out.println("Введенная строка не является ФИО");
+        }
     }
 }
